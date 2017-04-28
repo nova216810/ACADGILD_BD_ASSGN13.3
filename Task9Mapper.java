4b57fa2e4e5bd8c7fbdb2b3fb3e502ca7aec4755
nova216810 <ramya1286@gmail.com>
2017-04-28 17:31:56 +0530

    Add files via upload

---
+++ Task9Mapper.java
@@ -0,0 +1,23 @@
+package mapreduce.demo.task9;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*; 
+
+public class Task9Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	Text outKey = new Text();
+	IntWritable outValue = new IntWritable(1);
+	
+	public void map(LongWritable key, Text value, Context context) 
+			throws IOException, InterruptedException {
+		String[] words = value.toString().split(" ");
+		for (String word : words) {
+			outKey.set(word);
+			context.write(outKey, outValue);
+		}
+	}
+}
